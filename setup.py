bda3e1eb11cca164ddcf64b5260efd57f16f9139
Ubuntu <azunre@gmail.com>
2018-01-12 20:38:32 +0000

    typo in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='DukeThinClient',
     version='1.0.0',
     description='A thin client for interacting with dockerized duke primitive',
-    packages=['SimonThinClient'],
+    packages=['DukeThinClient'],
     install_requires=["numpy","pandas","requests","typing","gensim"],
     entry_points = {
         'd3m.primitives': [
